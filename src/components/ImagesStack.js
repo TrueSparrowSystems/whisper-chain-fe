4c90b4d7829bf14206c8c1d03ccd99501d56c0cc
Gopal Jasani <100685534+GopalJasani777@users.noreply.github.com>
2023-01-30 18:46:57 +0530

    Fixes

--- src/components/ImagesStack.js
+++ src/components/ImagesStack.js
@@ -94,7 +94,7 @@ const ImagesStack = ({ imageDetails: imageDetailsArray, pub }) => {
             src={
               imageDetailsArray[1]?.imageUrl
                 ? imageDetailsArray[1].imageUrl
-                : "https://i.picsum.photos/id/193/512/512.jpg?hmac=ay70CF2_XM0GJBcUQzMN6UNHDn0-kgSIu8KcENreNgM"
+                : "https://whisperchain-staging-static-files.s3.us-east-2.amazonaws.com/stability/image15.png"
             }
             sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
@@ -111,7 +111,7 @@ const ImagesStack = ({ imageDetails: imageDetailsArray, pub }) => {
             src={
               imageDetailsArray[2]?.imageUrl
                 ? imageDetailsArray[2].imageUrl
-                : "https://i.picsum.photos/id/524/512/512.jpg?hmac=2VlA0x6Y7osphrvDzR52TVkYjcc493rqcizd8HEXphY"
+                : "https://whisperchain-staging-static-files.s3.us-east-2.amazonaws.com/stability/image16.png"
             }
             sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
