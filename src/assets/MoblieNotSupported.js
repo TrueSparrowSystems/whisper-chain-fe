3a1ef8a7e34602e4f19d5f1da6c4e20edc17cfe8
Gopal Jasani <100685534+GopalJasani777@users.noreply.github.com>
2023-02-05 22:12:34 +0530

    errors fixed

--- src/assets/MoblieNotSupported.js
+++ src/assets/MoblieNotSupported.js
@@ -10,21 +10,21 @@ const MobileNotSupported = () => {
       <path
         d="M7.5 5.00001H12.5M4 4V16C4 17.1046 4.89543 18 6 18H14C15.1046 18 16 17.1046 16 16V4.00001C16 2.89544 15.1046 2.00001 14 2.00001L6 2C4.89543 2 4 2.89543 4 4ZM10 14H10.0708V14.0641H10V14Z"
         stroke="#666666"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M2 17.0254L16.5 2.52539"
         stroke="#666666"
-        stroke-width="1.5"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
       />
       <path
         d="M2 15.5L16.5 1"
         stroke="white"
-        stroke-width="1.5"
-        stroke-linecap="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
       />
     </svg>
   );
--- src/components/JoinedFromMobile.js
+++ src/components/JoinedFromMobile.js
@@ -37,9 +37,9 @@ export default function JoinedFromMobile() {
             <p className="font-bold pt-[20px]">This experience is best viewed on bigger screen sizes.</p>
             <div className="flex justify-center pt-[10px]">
               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7.5 5.00001H12.5M4 4V16C4 17.1046 4.89543 18 6 18H14C15.1046 18 16 17.1046 16 16V4.00001C16 2.89544 15.1046 2.00001 14 2.00001L6 2C4.89543 2 4 2.89543 4 4ZM10 14H10.0708V14.0641H10V14Z" stroke="#666666" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M2 17.0254L16.5 2.52539" stroke="#666666" stroke-width="1.5" stroke-linecap="round" />
-                <path d="M2 15.5L16.5 1" stroke="white" stroke-width="1.5" stroke-linecap="round" />
+                <path d="M7.5 5.00001H12.5M4 4V16C4 17.1046 4.89543 18 6 18H14C15.1046 18 16 17.1046 16 16V4.00001C16 2.89544 15.1046 2.00001 14 2.00001L6 2C4.89543 2 4 2.89543 4 4ZM10 14H10.0708V14.0641H10V14Z" stroke="#666666" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M2 17.0254L16.5 2.52539" stroke="#666666" strokeWidth="1.5" strokeLinecap="round" />
+                <path d="M2 15.5L16.5 1" stroke="white" strokeWidth="1.5" strokeLinecap="round" />
               </svg>
             </div>
           </div>
