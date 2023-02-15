337413e39408b317c1fa07d6d92f9f97ea5bd2da
Gopal Jasani <100685534+GopalJasani777@users.noreply.github.com>
2023-02-15 10:17:20 +0530

    onboarding

--- src/components/Onboarding.js
+++ src/components/Onboarding.js
@@ -47,6 +47,13 @@ const Onboarding = ({ publication, setOnBoarding }) => {
             src: "https://static.staging.whisperchain.xyz/whisperHomePage/onboarding-v3.mp4",
             type: "video/mp4"
         },
+        {
+            title:
+                "Join the fun",
+            subTitle: "Recreate the last image added to the chain by using the A.I. generation tool on our platform. ",
+            src: "https://static.staging.whisperchain.xyz/whisperHomePage/onboarding-v3.mp4",
+            type: "video/mp4"
+        },
     ];
 
     return (
@@ -61,12 +68,21 @@ const Onboarding = ({ publication, setOnBoarding }) => {
                 <source src="https://static.staging.whisperchain.xyz/whisperHomePage/onboarding-v2.mp4" type={onBoardingDetailsArray[index].type} />
                </video>
             }
-            { index > 1 &&
+            { index === 2 &&
                 <video autoPlay muted loop className="rounded-t-[20px]">
                 <source src="https://static.staging.whisperchain.xyz/whisperHomePage/onboarding-v3.mp4" type={onBoardingDetailsArray[index].type} />
                </video>
             }
-
+            { index === 3 &&
+                <video autoPlay muted loop className="rounded-t-[20px]">
+                <source src="https://static.staging.whisperchain.xyz/whisperHomePage/onboarding-v3.mp4" type={onBoardingDetailsArray[index].type} />
+               </video>
+            }
+            { index == 4 &&
+                <video autoPlay muted loop className="rounded-t-[20px]">
+                <source src="https://static.staging.whisperchain.xyz/whisperHomePage/onboarding-4.mp4" type={onBoardingDetailsArray[index].type} />
+               </video>
+            }
             <div className="p-[30px]">
                 <p className="text-[20px] font-bold text-[#000000]">{onBoardingDetailsArray[index].title}</p>
                 <p className="text-[16px] text-[#000000] opacity-80 pt-[5px]">{onBoardingDetailsArray[index].subTitle}</p>
@@ -76,15 +92,14 @@ const Onboarding = ({ publication, setOnBoarding }) => {
                             className={`${styles.nextButton}`}
                             onClick={() => {
                                 setIndex(index + 1);
-                                if (index === 3) {
+                                if (index === 4 ) {
                                     window.localStorage.setItem("onBoardingKey", false);
                                     setOnBoarding(false);
                                 }
                             }}>Next</button>
                     </div>
                 }
-                {index > 0 &&
-                         
+                {index > 0 && index <= 3 ?     
                     <div className="flex justify-between mt-[50px]">
                         <button
                             className={`${styles.backOnboard}`}
@@ -95,11 +110,24 @@ const Onboarding = ({ publication, setOnBoarding }) => {
                             className={`${styles.nextButtonOnboard}`}
                             onClick={() => {
                                 setIndex(index + 1);
-                                if (index === 3) {
+                                if (index === 4) {
                                     window.localStorage.setItem("onBoardingKey", false);
                                     setOnBoarding(false);
                                 }
                             }}>Next</button>
+                    </div>     : <div> </div>      
+                }
+                {index === 4 &&
+                    <div className="flex justify-center mt-[50px] tablet:mt-[20px] w-full">
+                        <button
+                            className={`${styles.nextButton}`}
+                            onClick={() => {
+                                setIndex(index + 1);
+                                if (index === 4) {
+                                    window.localStorage.setItem("onBoardingKey", false);
+                                    setOnBoarding(false);
+                                }
+                            }}>Get Started</button>
                     </div>
                 }
             </div>
