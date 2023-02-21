f590353d03663e59597d69e6926b5a51a80e7820
Gopal Jasani <100685534+GopalJasani777@users.noreply.github.com>
2023-02-21 12:15:19 +0530

    CTa added on video

---
+++ src/assets/PurpleGenerateIcon.js
@@ -0,0 +1,53 @@
+function PurpleGenerateLogo() {
+    return (
+        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M17.6055 10.1289L17.6055 13.8789" stroke="url(#paint0_radial_3107_12551)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M15.7305 12.0039L19.4805 12.0039" stroke="url(#paint1_radial_3107_12551)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M7.29297 3.25391L7.29297 6.37891" stroke="url(#paint2_radial_3107_12551)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.73047 4.81641L8.85547 4.81641" stroke="url(#paint3_radial_3107_12551)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M13.8555 14.5039L13.8555 17.0039" stroke="url(#paint4_radial_3107_12551)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M12.6055 15.7539L15.1055 15.7539" stroke="url(#paint5_radial_3107_12551)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M15.2958 3.06942L3.67827 14.687C3.43419 14.931 3.43419 15.3268 3.67827 15.5708L5.29412 17.1867C5.5382 17.4308 5.93392 17.4308 6.178 17.1867L17.7955 5.56915C18.0396 5.32508 18.0396 4.92935 17.7955 4.68527L16.1797 3.06942C15.9356 2.82534 15.5399 2.82534 15.2958 3.06942Z" stroke="url(#paint6_radial_3107_12551)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.9805 6.37891L14.4805 8.87891" stroke="url(#paint7_radial_3107_12551)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<defs>
+<radialGradient id="paint0_radial_3107_12551" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(18.1055 13.1646) rotate(-90) scale(4.01786 1.07143)">
+<stop stop-color="#6F1AFF"/>
+<stop offset="1" stop-color="#C6A4FF"/>
+</radialGradient>
+<radialGradient id="paint1_radial_3107_12551" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(17.6055 12.8134) rotate(-90) scale(1.07143 4.01786)">
+<stop stop-color="#6F1AFF"/>
+<stop offset="1" stop-color="#C6A4FF"/>
+</radialGradient>
+<radialGradient id="paint2_radial_3107_12551" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(7.79297 5.78367) rotate(-90) scale(3.34821 1.07143)">
+<stop stop-color="#6F1AFF"/>
+<stop offset="1" stop-color="#C6A4FF"/>
+</radialGradient>
+<radialGradient id="paint3_radial_3107_12551" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(7.29297 5.62593) rotate(-90) scale(1.07143 3.34821)">
+<stop stop-color="#6F1AFF"/>
+<stop offset="1" stop-color="#C6A4FF"/>
+</radialGradient>
+<radialGradient id="paint4_radial_3107_12551" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(14.3555 16.5277) rotate(-90) scale(2.67857 1.07143)">
+<stop stop-color="#6F1AFF"/>
+<stop offset="1" stop-color="#C6A4FF"/>
+</radialGradient>
+<radialGradient id="paint5_radial_3107_12551" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(13.8555 16.5634) rotate(-90) scale(1.07143 2.67857)">
+<stop stop-color="#6F1AFF"/>
+<stop offset="1" stop-color="#C6A4FF"/>
+</radialGradient>
+<radialGradient id="paint6_radial_3107_12551" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(11.5106 10.9018) rotate(-135) scale(3.78767 18.9425)">
+<stop stop-color="#6F1AFF"/>
+<stop offset="1" stop-color="#C6A4FF"/>
+</radialGradient>
+<radialGradient id="paint7_radial_3107_12551" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(13.2305 8.40272) rotate(-90) scale(2.67857)">
+<stop stop-color="#6F1AFF"/>
+<stop offset="1" stop-color="#C6A4FF"/>
+</radialGradient>
+</defs>
+</svg>
+
+        
+    );
+  }
+  
+  export default PurpleGenerateLogo;
+  
