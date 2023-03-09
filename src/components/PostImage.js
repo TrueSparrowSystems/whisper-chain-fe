ded0330fc60fe01f08b18e2f2ea6260c4a8ecdb2
Gopal Jasani <100685534+GopalJasani777@users.noreply.github.com>
2023-03-09 15:36:07 +0530

    Hover state on view on lens

--- src/components/PostImage.js
+++ src/components/PostImage.js
@@ -311,7 +311,7 @@ export const PostImage = ({ imageDetails, chainId }) => {
                   onClick={() => {
                     window.open(imageDetails.lensterPostUrl, "_blank");
                   }}
-                  className={`flex items-center relative bottom-[10px] p-[10px] justify-center `}
+                  className={`flex items-center relative bottom-[10px] p-[10px] justify-center hover:opacity-60`}
                 >
                   <WhiteEyeIcon />{" "}
                   <span className="ml-[10px] text-white opacity-[0.6]">
