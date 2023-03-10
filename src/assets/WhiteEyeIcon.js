cb81bba39b1c2fa02d51ec02751e6dff62652394
Gopal Jasani <100685534+GopalJasani777@users.noreply.github.com>
2023-03-10 12:17:21 +0530

    view on lens hover state changed

--- src/assets/WhiteEyeIcon.js
+++ src/assets/WhiteEyeIcon.js
@@ -10,7 +10,7 @@ const WhiteEyeIcon = () => {
       <path
         d="M10 4.375C3.75 4.375 1.25 10 1.25 10C1.25 10 3.75 15.625 10 15.625C16.25 15.625 18.75 10 18.75 10C18.75 10 16.25 4.375 10 4.375Z"
         stroke="white"
-        strokeOpacity="0.6"
+        strokeOpacity="0.8"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -18,7 +18,7 @@ const WhiteEyeIcon = () => {
       <path
         d="M10 13.125C11.7259 13.125 13.125 11.7259 13.125 10C13.125 8.27411 11.7259 6.875 10 6.875C8.27411 6.875 6.875 8.27411 6.875 10C6.875 11.7259 8.27411 13.125 10 13.125Z"
         stroke="white"
-        strokeOpacity="0.6"
+        strokeOpacity="0.8"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
